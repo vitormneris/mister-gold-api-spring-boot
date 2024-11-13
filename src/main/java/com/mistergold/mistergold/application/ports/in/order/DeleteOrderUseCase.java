d4bc15da810feed1ab416dc0c2d619a715602693
vitormneris <joaomoreiraneris0@gmail.com>
2024-11-12 21:42:46 -0300

    Cliente podem ter lista de pedidos ao inves de um unico pedido

--- src/main/java/com/mistergold/mistergold/application/ports/in/order/DeleteOrderUseCase.java
+++
@@ -1,8 +0,0 @@
-package com.mistergold.mistergold.application.ports.in.order;
-
-import com.mistergold.mistergold.application.domain.order.Order;
-
-public interface DeleteOrderUseCase {
-    void delete(String id);
-    Order inactivate(String id);
-}
--- src/main/java/com/mistergold/mistergold/application/ports/out/order/DeleteOrderPort.java
+++
@@ -1,8 +0,0 @@
-package com.mistergold.mistergold.application.ports.out.order;
-
-import com.mistergold.mistergold.application.domain.order.Order;
-
-public interface DeleteOrderPort {
-    void delete(String id);
-    Order inactivate(String id);
-}
